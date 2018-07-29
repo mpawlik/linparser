ef3f77867f741932221ac4d2c39e4ce1307cc857
Maciej Pawlik <m.pawlik@cyfronet.pl>
2018-07-29 20:08:34 +0200

    Add aws 3008

--- score_histogram.py
+++ score_histogram.py
@@ -23,8 +23,8 @@ def set_hist_params(ax):
 
 def set_scatter_params(ax):
     ax.set_ylim(0, 45)
-    ax.set_xlim(0, 2304)
-    ax.xaxis.set_ticks([0, 512, 1024, 1536, 2048])
+    ax.set_xlim(0, 3100)
+    ax.xaxis.set_ticks([0, 512, 1024, 1536, 2048, 3072])
     ax.yaxis.set_ticks(np.arange(0, 45, 5))
     ax.grid(alpha=0.3)
     ax.set_xlabel("Memory size [MB]")
@@ -44,36 +44,43 @@ if __name__ == '__main__':
     aws_mem_1024 = convert_to_list(aws_mem[1024], 'score')
     aws_mem_1536 = convert_to_list(aws_mem[1536], 'score')
     aws_mem_2048 = convert_to_list(aws_mem[2048], 'score')
+    aws_mem_3008 = convert_to_list(aws_mem[3008], 'score')
+
+    print "max_aws:", max(aws_mem_256 + aws_mem_512 + aws_mem_1024 + aws_mem_1536 + aws_mem_2048 + aws_mem_3008)
 
     gcf_mem = section_on_property(results['gcf'], 'fun_size')
 
-    # gcf_mem_128 = convert_to_list(gcf_mem[128], 'score')
     gcf_mem_256 = convert_to_list(gcf_mem[256], 'score')
     gcf_mem_512 = convert_to_list(gcf_mem[512], 'score')
     gcf_mem_1024 = convert_to_list(gcf_mem[1024], 'score')
     gcf_mem_2048 = convert_to_list(gcf_mem[2048], 'score')
+
+    print "max_gcf:", max(gcf_mem_256 + gcf_mem_512 + gcf_mem_1024 + gcf_mem_2048)
 
     ibm_mem = section_on_property(results['ibm'], 'fun_size')
 
     ibm_mem_256 = convert_to_list(ibm_mem[256], 'score')
     ibm_mem_512 = convert_to_list(ibm_mem[512], 'score')
 
+    print "max_ibm:", max(ibm_mem_256 + ibm_mem_512)
+
     bins = np.linspace(0, 50, 100)
 
-    fig = plt.figure(figsize=(14, 5))
+    fig = plt.figure(figsize=(14, 7))
 
-    outer = gridspec.GridSpec(2, 3, wspace=0.4, hspace=0.3)
+    outer = gridspec.GridSpec(2, 3, wspace=0.4, hspace=0.3, height_ratios=[1, 1.5])
 
     # Scatter >>>>>>>>>>>>>>
 
     ax = plt.subplot(outer[0])
+    dot_alpha = 0.01
     ax.scatter(
         [256] * len(aws_mem_256) + [512] * len(aws_mem_512) + [1024] * len(aws_mem_1024) + [1536] * len(
-            aws_mem_1536) + [2048] * len(aws_mem_2048),
-        aws_mem_256 + aws_mem_512 + aws_mem_1024 + aws_mem_1536 + aws_mem_2048,
-        alpha=0.1,
+            aws_mem_1536) + [2048] * len(aws_mem_2048) + [3008] * len(aws_mem_3008),
+        aws_mem_256 + aws_mem_512 + aws_mem_1024 + aws_mem_1536 + aws_mem_2048 + aws_mem_3008,
+        alpha=dot_alpha,
         c=["C0"] * len(aws_mem_256) + ["C1"] * len(aws_mem_512) + ["C2"] * len(aws_mem_1024) + ["C3"] * len(
-            aws_mem_1536) + ["C4"] * len(aws_mem_2048)
+            aws_mem_1536) + ["C4"] * len(aws_mem_2048) + ["C5"] * len(aws_mem_3008)
     )
     ax.set_title("AWS")
     set_scatter_params(ax)
@@ -82,7 +89,7 @@ if __name__ == '__main__':
     ax.scatter(
         [256] * len(gcf_mem_256) + [512] * len(gcf_mem_512) + [1024] * len(gcf_mem_1024) + [2048] * len(gcf_mem_2048),
         gcf_mem_256 + gcf_mem_512 + gcf_mem_1024 + gcf_mem_2048,
-        alpha=0.1,
+        alpha=dot_alpha,
         c=["C0"] * len(gcf_mem_256) + ["C1"] * len(gcf_mem_512) + ["C2"] * len(gcf_mem_1024) + ["C4"] * len(
             gcf_mem_2048)
     )
@@ -93,7 +100,7 @@ if __name__ == '__main__':
     ax.scatter(
         [256] * len(ibm_mem_256) + [512] * len(ibm_mem_512),
         ibm_mem_256 + ibm_mem_512,
-        alpha=0.1,
+        alpha=dot_alpha,
         c=["C0"] * len(ibm_mem_256) + ["C1"] * len(ibm_mem_512),
     )
     ax.set_title("IBM")
@@ -103,7 +110,7 @@ if __name__ == '__main__':
 
     # AWS
 
-    gs_aws_l = gridspec.GridSpecFromSubplotSpec(5, 1, subplot_spec=outer[3], hspace=.0)
+    gs_aws_l = gridspec.GridSpecFromSubplotSpec(6, 1, subplot_spec=outer[3], hspace=.0)
 
     ax0 = plt.subplot(gs_aws_l[0])
     ax0.hist(aws_mem_256, bins, alpha=0.5, edgecolor='k', label='256', color='C0')
@@ -125,11 +132,15 @@ if __name__ == '__main__':
     ax4 = plt.subplot(gs_aws_l[4])
     ax4.hist(aws_mem_2048, bins, alpha=0.5, edgecolor='k', label='2048', color='C4')
     set_hist_params(ax4)
-    ax4.set_xlabel("Performance [GFlops]")
+
+    ax5 = plt.subplot(gs_aws_l[5])
+    ax5.hist(aws_mem_3008, bins, alpha=0.5, edgecolor='k', label='2048', color='C5')
+    set_hist_params(ax5)
+    ax5.set_xlabel("Performance [GFlops]")
 
     # GCF
 
-    gs_gcf_l = gridspec.GridSpecFromSubplotSpec(5, 1, subplot_spec=outer[4], hspace=.0)
+    gs_gcf_l = gridspec.GridSpecFromSubplotSpec(6, 1, subplot_spec=outer[4], hspace=.0)
 
     ax0 = plt.subplot(gs_gcf_l[0])
     ax0.hist(gcf_mem_256, bins, alpha=0.5, edgecolor='k', label='256', color='C0')
@@ -149,7 +160,7 @@ if __name__ == '__main__':
     set_hist_params(ax4)
     ax4.set_xlabel("Performance [GFlops]")
 
-    gs_ibm_l = gridspec.GridSpecFromSubplotSpec(5, 1, subplot_spec=outer[5], hspace=.0)
+    gs_ibm_l = gridspec.GridSpecFromSubplotSpec(6, 1, subplot_spec=outer[5], hspace=.0)
 
     ax0 = plt.subplot(gs_ibm_l[0])
     ax0.hist(ibm_mem_256, bins, alpha=0.5, edgecolor='k', label='256', color='C0')
