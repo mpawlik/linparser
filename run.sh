c5b8cc7b1513d61572c107d10ec98529b4a51aa3
Maciej Pawlik <m.pawlik@cyfronet.pl>
2018-08-06 22:45:16 +0200

    Add run.sh.

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python delay_histogram.py results/*
+python score_histogram.py results/*
+python overhead_request_response_histogram.py results/*
+python wasted_time.py results/*
+python gantt_chart.py results/*
